23a79560ec6f3ef6888c59bc7174d43c339cbb30
nathan <nathan.soulier26@gmail.com>
2021-06-15 06:33:50 -0700

    ContactScreen & darkMode set to true by default

--- app/screens/ContactScreen.vue
+++ app/screens/ContactScreen.vue
@@ -1,13 +1,46 @@
 <template>
   <view :style="styles('page')">
-    <text class="heading">Contact</text>
+    <TouchableOpacity @press="goTo('https://emotion-b-sport.com')">
+      <image
+        :source="darkMode ? imgs.logoVertivalDark : imgs.logoVertivalLight"
+        :style="styles('contactScreen.logo')"
+      />
+    </TouchableOpacity>
+    <TouchableOpacity  :style="styles('contactScreen.mail')" @press="goTo('mailto:'+ mail)">
+      <FontAwesome5 name="envelope" :style="styles('contactScreen.envelope')" solid/>
+      <text :style="styles('contactScreen.mailText')">{{ mail }}</text>
+    </TouchableOpacity>
+    <view :style="styles('contactScreen.socials')">
+      <TouchableOpacity @press="goTo('https://www.facebook.com/emotionbsport/')">
+        <FontAwesome5 name="facebook" :style="[styles('contactScreen.socialLogo'), {color: '#4267B2'}]" solid/>
+      </TouchableOpacity>
+      <view :style="styles('contactScreen.line')"/>
+      <TouchableOpacity @press="goTo('https://twitter.com/J0hn_Isaiah')">
+        <FontAwesome5 name="twitter" :style="[styles('contactScreen.socialLogo'), {color: '#1DA1F2'}]" solid/>
+      </TouchableOpacity>
+      <view :style="styles('contactScreen.line')"/>
+      <TouchableOpacity @press="goTo('https://www.youtube.com/channel/UCiIn6sfX7divZfaKDVS45nw')">
+        <FontAwesome5 name="youtube" :style="[styles('contactScreen.socialLogo'), {color: '#FF0000'}]" solid/>
+      </TouchableOpacity>
+    </view>
   </view>
 </template>
 
 <script>
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import { Linking } from 'react-native';
+
 export default {
+  components:{
+    FontAwesome5
+  },
   props: {
     navigation: { type: Object }
+  },
+  data(){
+    return {
+      mail: "contact@emotion-b-sport.com"
+    }
   },
   computed:{
     user(){
@@ -16,25 +49,53 @@ export default {
     styles() {
       return this.$styles;
     },
+    imgs() {
+      return this.$imgs;
+    },
+    darkMode(){
+      return this.$store.state.darkMode;
+    }
+  },
+  methods: {
+    goTo(url){
+      console.log(url);
+      Linking.openURL(url);
+    },
   }
 }
 </script>
 
 <style>
-.container {
+.social-logo{
+  font-size: 72px;
+}
+.line {
+  width: 25px;
+  height:2px;
+  margin-right: 10px;
+  margin-left: 10px;
+  background-color: white;
+}
+.socials {
+  display: flex;
+  flex-direction: row;
   align-items: center;
-  justify-content: center;
-  flex: 1;
-  padding: 50px;
+  align-self: center;
 }
-.heading {
-  font-size: 30px;
-  font-weight: bold;
-  color: blue;
-  margin: 20px;
+.envelope{
+  color:white;
+  font-size: 52;
+  margin-right: 18;
 }
-.text {
-  text-align: center;
-  margin: 10px;
+.mail{
+  align-self: center;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin-bottom: 80;
+}
+.mail-text{
+  font-size:16px;
+  color: white;
 }
 </style>
